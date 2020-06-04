f80daed3ef796b715e77cf89f4c08efd11649f57
Olivier Vincent <olivier.vincent@univ-lyon1.fr>
2020-06-04 20:01:48 +0200

    Add package description in __init__.py

--- gittools/__init__.py
+++ gittools/__init__.py
@@ -1,4 +1,11 @@
-"""Git tools in python."""
+"""Tools for using GIT in python, based on gitpython.
+
+Functions
+---------
+The module is designed to use mainly the function current_commit_hash()
+but path_in_tree() is also made available in case it proves useful. See
+help of these functions and README file for documentation and examples.
+"""
 
 from .gittools import current_commit_hash, path_in_tree
 
