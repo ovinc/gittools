8261256db497140f90a216aeee41e2aa686d1009
Olivier Vincent <olivier.vincent@univ-lyon1.fr>
2021-01-12 19:23:48 +0100

    Define __version__ automatically in __init__.py

--- gittools/__init__.py
+++ gittools/__init__.py
@@ -4,3 +4,7 @@ from .gittools import DirtyRepo, NotInTree
 from .gittools import current_commit_hash, path_status, module_status
 from .gittools import repo_tags, path_in_tree
 from .gittools import save_metadata
+
+from importlib.metadata import version
+
+__version__ = version('gittools')
